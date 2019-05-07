9606f180f76b6370566deab4a66b5a53b7b2040c
Ramazan <laumanov.r@gmail.com>
2019-05-07 14:46:17 +0600

    added diagram modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { DiagramComponent } from './components/diagram/diagram.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { DiagramService } from './services/diagram.service';
+import { FormsModule } from '@angular/forms';
+
+export const appRoutes: Routes = [
+  {path: '', component: DiagramComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiagramComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  exports: [AppComponent, DiagramComponent],
+  providers: [DiagramService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
